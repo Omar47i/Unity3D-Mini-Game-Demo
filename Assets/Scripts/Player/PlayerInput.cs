2ff14c9bd90a6de9d0270848feea89230bcfbc49
Omar47i <os1991ai@gmail.com>
2017-12-01 17:56:02 +0200

    Add Player gameObject and Input controls

---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    Rigidbody rb;
+    float speed = 10f;
+
+    float horAxis;
+    float verAxis;
+
+    void Awake()
+    {
+        print(Random.insideUnitCircle.ToString());
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float horAxis = Input.GetAxis("Horizontal");
+        float verAxis = Input.GetAxis("Vertical");
+        
+        Vector3 moveDir = transform.TransformDirection(horAxis, 0f, verAxis);
+
+        rb.AddForce(moveDir * 70f);
+    }
+}
